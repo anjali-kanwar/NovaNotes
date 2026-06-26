23e099a84e45a8d877912c1cb08abe11d3d3296b
Anjali Kanwar <anjalikanwar006@gmail.com>
2026-06-26 09:29:47 +0530

    production->development

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT=process.env.PORT || 5001;
 const __dirname=path.resolve();
 
 // middleware
-if (process.env.NODE_ENV !== "production") 
+if (process.env.NODE_ENV !== "development") 
 {
     app.use(cors({
     origin: "http://localhost:5173",
@@ -35,7 +35,7 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
